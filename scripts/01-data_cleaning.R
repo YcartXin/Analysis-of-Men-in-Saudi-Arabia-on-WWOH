b8020eda9cccd177895a5d3d3d9c59869b0ad6f3
Tracy Yang <ycart.yang@mail.utoronto.ca>
2024-02-14 18:02:23 -0500

    Added new online national data set into scripts and paper; updated the Data section

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -13,15 +13,16 @@ library(dplyr)
 library(haven)
 
 #### Clean data ####
-raw_data <- read_dta("data/raw_data/01_main_exp_clean.dta")
+mainexp_raw_data <- read_dta("data/raw_data/01_main_exp_clean.dta")
+online_raw_data <- read_dta("data/raw_data/03_1st_online_svy_clean.dta")
 
 #taking out NA data from age variable as well as single men in the survey.
-cleaned_data <- raw_data |> clean_names() |> na.omit(age)|> mutate(
+main_cleaned <- raw_data |> clean_names() |> na.omit(age)|> mutate(
   age_groups = case_when(
   age <= 23 ~ 1,
   age >= 30 ~ 3,
   TRUE ~ 2)) |> filter(married == 1)
 
-
+online_cleaned
 #### Save data ####
 write_csv(cleaned_data, "data/analysis_data/cleaned_data.csv")
