35062b75a9a2a7afaee87bd031b7f976d62cd5ef
Tracy Yang <ycart.yang@mail.utoronto.ca>
2024-02-15 02:16:43 -0500

    Added and cleaned a new data set for follow up to the main experiment.

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -14,15 +14,28 @@ library(haven)
 
 #### Clean data ####
 mainexp_raw_data <- read_dta("data/raw_data/01_main_exp_clean.dta")
+follow_raw_data <- read_dta("data/raw_data/02_follow_up_clean.dta")
 online_raw_data <- read_dta("data/raw_data/03_1st_online_svy_clean.dta")
+sec_raw_data <- read_dta("data/raw_data/04_2nd_online_svy_clean.dta")
 
 #taking out NA data from age variable as well as single men in the survey.
-main_cleaned <- raw_data |> clean_names() |> na.omit(age)|> mutate(
+main_cleaned <- mainexp_raw_data |> clean_names() |> 
+  na.omit(age)|> mutate(
   age_groups = case_when(
   age <= 23 ~ 1,
   age >= 30 ~ 3,
   TRUE ~ 2)) |> filter(married == 1)
 
-online_cleaned
+follow_cleaned <- follow_raw_data |> clean_names() |> 
+  na.omit(age) |>
+  select(employed_3mos_out_fl, employed_now_out_fl) |> na.omit() 
+
+online_cleaned <- online_raw_data |> clean_names() |> 
+  select(c_outside_self)|>
+  na.omit()
+
 #### Save data ####
-write_csv(cleaned_data, "data/analysis_data/cleaned_data.csv")
+write_csv(main_cleaned, "data/analysis_data/main_cleaned.csv")
+write_csv(follow_cleaned, "data/analysis_data/follow_cleaned.csv")
+write_csv(online_cleaned, "data/analysis_data/online_cleaned.csv")
+
